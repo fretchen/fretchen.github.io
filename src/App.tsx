6406d6269837f772496b0b85f2723bd39a946a70
fretchen <fred.jendrzejewski@gmail.com>
2024-12-25 08:51:25 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,6 @@ import * as React from "react";
 export default () => (
   <>
     <h1>Welcome to React Vite Micro App!</h1>
-    <p>Hard to get more minimal than this React app.</p>
+    <p>Hard to get more minimal than this small React app.</p>
   </>
 );

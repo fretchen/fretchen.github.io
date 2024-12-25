92a83369580e87c76002440db0c6dc9f568ed414
fretchen <polka.99gefiedert@icloud.com>
2024-12-25 16:29:34 +0100

    Make it possible to have markdown

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import * as React from "react";
 
+import { MuiMarkdown } from 'mui-markdown';
+
 export default () => (
   <>
     <h1>Welcome to React Vite Micro App!</h1>
     <p>Hard to get more minimal than this small React app.</p>
+    <MuiMarkdown>{`# Hello markdown!`}</MuiMarkdown>;
   </>
 );

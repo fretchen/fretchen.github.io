fcc7293829c668f4974332d96a78519a2cc3eee6
fretchen <polka.99gefiedert@icloud.com>
2024-12-26 16:24:39 +0100

    Load some blog posts

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,23 @@
 import * as React from "react";
 
 import { MuiMarkdown } from "mui-markdown";
+import blogs  from "../blog/blogs.json"; 
 
-const App: React.FC = () => (
-  <>
+const App: React.FC = function() {
+  
+  console.log(blogs);
+  return (
+  <div className="App">
     <h1>Welcome to React Vite Micro App!</h1>
     <p>Hard to get more minimal than this small React app.</p>
-    <MuiMarkdown>{`# Hello markdown!`}</MuiMarkdown>
-  </>
-);
+    {blogs.map((blog, index) => (
+        <div key={index}>
+          <h2>{blog.title}</h2>
+          <MuiMarkdown>{blog.content}</MuiMarkdown>
+        </div>
+      ))}
+  </div>
+  );
+};
 
 export default App;

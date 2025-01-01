bd64baa036a6ee1ad9bd9f97d75eaa161b67789d
fretchen <fred.jendrzejewski@gmail.com>
2025-01-01 17:28:18 +0100

    Clean the layout

--- pages/blog/+Page.tsx
+++ pages/blog/+Page.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 
 import blogs from "../../blog/blogs.json";
-import { Link } from "../../components/Link";
+import BlogList from "../../components/BlogList";
 
 const App: React.FC = function () {
   console.log(blogs);
@@ -9,15 +9,7 @@ const App: React.FC = function () {
     <div className="Blog">
       <h1>Welcome to my blog!</h1>
       <p>It contains notes about all kind of topic, ideas etc.</p>
-      {blogs.map((blog, index) => (
-        <div key={index}>
-          <h2>{blog.title}</h2>
-          <p>
-            {" "}
-            <Link href={`/blog/${index}`}>Read more</Link>
-          </p>
-        </div>
-      ))}
+      <BlogList />
     </div>
   );
 };

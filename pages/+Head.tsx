a135a0cd4d467692a4c29cb6e655c7ab1e5bf53c
fretchen <fred.jendrzejewski@gmail.com>
2025-01-03 10:30:24 +0100

    Umami

--- pages/+Head.tsx
+++ pages/+Head.tsx
@@ -6,6 +6,14 @@ export default function HeadDefault() {
   return (
     <>
       <link rel="icon" href={" "} />
+      {
+        // umami analytics script
+      }
+      <script
+        defer
+        src="https://cloud.umami.is/script.js"
+        data-website-id="e41ae7d9-a536-426d-b40e-f2488b11bf95"
+      ></script>
     </>
   );
 }

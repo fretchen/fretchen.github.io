57acc09f5e436b30f005cd8a35c5e1eadad210ee
fretchen <polka.99gefiedert@icloud.com>
2025-01-06 20:57:55 +0100

    Fix a bug on the client side

--- pages/blog/+Page.tsx
+++ pages/blog/+Page.tsx
@@ -1,10 +1,8 @@
 import * as React from "react";
 
-import blogs from "../../blog/blogs.json";
 import BlogList from "../../components/BlogList";
 
 const App: React.FC = function () {
-  console.log(blogs);
   return (
     <div className="Blog">
       <h1>Welcome to my blog!</h1>

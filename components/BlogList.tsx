ce122ed6f91ad087537ffa4bb0db0e93aee9ffd4
fretchen <fred.jendrzejewski@gmail.com>
2025-01-06 21:04:30 +0100

    Update BlogList.tsx

--- components/BlogList.tsx
+++ components/BlogList.tsx
@@ -6,7 +6,7 @@ import { Link } from "./Link";
 const BlogList: React.FC = function () {
   return (
     <div className="BlogList">
-      {blogs.reverse().map((blog, index) => (
+      {[...blogs].reverse().map((blog, index) => (
         <div key={blogs.length - 1 - index} style={{ marginBottom: "20px" }}>
           {blog.publishing_date && <p style={{ marginBottom: "5px" }}>{blog.publishing_date}</p>}
           <Link href={`/blog/${blogs.length - 1 - index}`}>

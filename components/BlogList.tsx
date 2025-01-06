beca2948e37e0b9c7e611c52590dd69329eeb693
fretchen <fred.jendrzejewski@gmail.com>
2025-01-06 20:57:55 +0100

    Fix a bug on the client side

--- components/BlogList.tsx
+++ components/BlogList.tsx
@@ -4,11 +4,9 @@ import blogs from "../blog/blogs.json";
 import { Link } from "./Link";
 
 const BlogList: React.FC = function () {
-  // invert the presented order of the blogs
-  const invertedBlogs = blogs.reverse();
   return (
     <div className="BlogList">
-      {invertedBlogs.map((blog, index) => (
+      {blogs.reverse().map((blog, index) => (
         <div key={blogs.length - 1 - index} style={{ marginBottom: "20px" }}>
           {blog.publishing_date && <p style={{ marginBottom: "5px" }}>{blog.publishing_date}</p>}
           <Link href={`/blog/${blogs.length - 1 - index}`}>

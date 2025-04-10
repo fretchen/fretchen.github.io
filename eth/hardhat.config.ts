4c80e4a976918ed2720a4aca6898ecb4539572e0
fretchen <polka.99gefiedert@icloud.com>
2025-04-10 22:48:29 +0200

    Verified contract on Sepolia and OP Sepolia

--- eth/hardhat.config.ts
+++ eth/hardhat.config.ts
@@ -1,12 +1,12 @@
 import type { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox-viem";
-
+import "@nomicfoundation/hardhat-verify";
 import { vars } from "hardhat/config";
 
 const ALCHEMY_API_KEY = vars.get("ALCHEMY_API_KEY");
 const SEPOLIA_PRIVATE_KEY = vars.get("SEPOLIA_PRIVATE_KEY");
 const ETHERSCAN_API_KEY = vars.get("ETHERSCAN_API_KEY");
-
+const OPTIMISTIC_ETHERSCAN_API_KEY = vars.get("OPTIMISTIC_ETHERSCAN_API_KEY");
 const config: HardhatUserConfig = {
   solidity: "0.8.28",
   networks: {
@@ -20,7 +20,17 @@ const config: HardhatUserConfig = {
     },
   },
   etherscan: {
-    apiKey: ETHERSCAN_API_KEY,
+    apiKey: { seoplia: ETHERSCAN_API_KEY, optsepolia: OPTIMISTIC_ETHERSCAN_API_KEY },
+    customChains: [
+      {
+        network: "optsepolia",
+        chainId: 11155420,
+        urls: {
+          apiURL: "https://api-sepolia-optimistic.etherscan.io/api",
+          browserURL: "https://sepolia-optimism.etherscan.io/",
+        },
+      },
+    ],
   },
 };
 

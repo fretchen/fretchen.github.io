86ffb730eec69a9a8b22f1d369164bbd83d85322
fretchen <polka.99gefiedert@icloud.com>
2025-04-11 18:30:44 +0200

    Update chainlink.ts

--- website/utils/chainlink.ts
+++ website/utils/chainlink.ts
@@ -1,21 +1,21 @@
 // This functions get details about Star Wars characters. This example will showcase usage of HTTP requests and console.logs.
 // 1, 2, 3 etc.
-const characterId = args[0]
-console.log(characterId)
+const characterId = args[0];
+console.log(characterId);
 // Execute the API request (Promise)
 const apiResponse = await Functions.makeHttpRequest({
   url: `https://mypersonalcloudmaqsyplo-myhelloworld.functions.fnc.fr-par.scw.cloud/`,
-  params: {prompt: characterId},
-})
+  params: { prompt: characterId },
+});
 
 if (apiResponse.error) {
-  console.error(apiResponse.error)
-  throw Error("Request failed")
+  console.error(apiResponse.error);
+  throw Error("Request failed");
 }
 
 const { data } = apiResponse;
 
-console.log('API response data:', JSON.stringify(data, null, 2));
+console.log("API response data:", JSON.stringify(data, null, 2));
 
 // Return Character Name
-return Functions.encodeString(data.message)
+return Functions.encodeString(data.message);

c242df73bef9590146a4ea24450f352304418c36
fretchen <8323674+fretchen@users.noreply.github.com>
2025-05-18 20:33:15 +0200

    Clean wallet buttons (#54)
    * SImpler walletoptions
    
    * lint it
    
    * Update SupportArea.tsx
    
    ---------
    
    Co-authored-by: fretchen <polka.99gefiedert@icloud.com>

--- website/panda.config.ts
+++ website/panda.config.ts
@@ -24,6 +24,7 @@ export default defineConfig({
           border: { value: "#eeeeee" },
           text: { value: "#333333" },
           background: { value: "#ffffff" },
+          light: { value: "#ffffff" },
         },
         spacing: {
           "0": { value: "0px" },

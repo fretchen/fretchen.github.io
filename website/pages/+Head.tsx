23d187a31e486f3469f12f0837f1705d752ebef5
fretchen <polka.99gefiedert@icloud.com>
2025-06-06 17:14:46 +0200

    Update +Head.tsx

--- website/pages/+Head.tsx
+++ website/pages/+Head.tsx
@@ -6,14 +6,14 @@ export default function HeadDefault() {
   return (
     <>
       <link rel="icon" href={favicon} />
-      {
-        // umami analytics script
-      }
-      <script
-        defer
-        src="https://cloud.umami.is/script.js"
-        data-website-id="e41ae7d9-a536-426d-b40e-f2488b11bf95"
-      ></script>
+      {/* umami analytics script - disabled when DISABLE_ANALYTICS is set */}
+      {!import.meta.env.VITE_DISABLE_ANALYTICS && (
+        <script
+          defer
+          src="https://cloud.umami.is/script.js"
+          data-website-id="e41ae7d9-a536-426d-b40e-f2488b11bf95"
+        ></script>
+      )}
     </>
   );
 }

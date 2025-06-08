a3be6c77ac13c7fe7dfa10fe248247795e97ad52
fretchen <polka.99gefiedert@icloud.com>
2025-06-08 17:10:41 +0200

    Make it simpler with the components

---
+++ website/components/Tab.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { TabProps } from "../types/components";
+import * as styles from "../layouts/styles";
+
+// Tab Component
+export function Tab({ label, isActive, onClick }: TabProps) {
+  return (
+    <button className={`${styles.tabs.tab} ${isActive ? styles.tabs.activeTab : ""}`} onClick={onClick}>
+      {label}
+    </button>
+  );
+}

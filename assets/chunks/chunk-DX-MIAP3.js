0fc0e2fbb61abb88e2784f69bb9661041a6438c4
fretchen <8323674+fretchen@users.noreply.github.com>
2025-06-18 19:19:19 +0000

    Deploying to gh-pages from @ fretchen/fretchen.github.io@4d3120c9cff55dd69d099c2032f2070349d14ed4 🚀

--- assets/chunks/chunk-DX-MIAP3.js
+++
@@ -1,8 +0,0 @@
-import{m as o}from"./chunk-CE9YgSfa.js";import"./chunk-B7I0Dq53.js";import"./chunk-BapEKB3C.js";import"./chunk-DWWfR5ae.js";/* empty css              *//* empty css              *//* empty css              */const n=o`<svg fill="none" viewBox="0 0 16 16">
-  <path
-    fill="currentColor"
-    fill-rule="evenodd"
-    d="M1.46 4.96a1 1 0 0 1 1.41 0L8 10.09l5.13-5.13a1 1 0 1 1 1.41 1.41l-5.83 5.84a1 1 0 0 1-1.42 0L1.46 6.37a1 1 0 0 1 0-1.41Z"
-    clip-rule="evenodd"
-  />
-</svg>`;export{n as chevronBottomSvg};

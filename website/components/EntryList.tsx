ad48b7d7498f44e8b69cdfabb1ecff7bb116d180
fretchen <polka.99gefiedert@icloud.com>
2025-06-18 17:23:04 +0200

    Cleaner images

--- website/components/EntryList.tsx
+++ website/components/EntryList.tsx
@@ -1,12 +1,11 @@
 import * as React from "react";
 import { Link } from "./Link";
-import { NFTPreviewImage } from "./NFTPreviewImage";
 import { EntryNftImage } from "./EntryNftImage";
 import { EntryListProps } from "../types/components";
 import { entryList } from "../layouts/styles";
 
 /**
- * Component that renders a list of blog entries with links and subtle image indicators
+ * Component that renders a list of blog entries with NFT images and links
  */
 const EntryList: React.FC<EntryListProps> = ({
   blogs,
@@ -17,20 +16,6 @@ const EntryList: React.FC<EntryListProps> = ({
   limit,
   showViewAllLink = false,
 }) => {
-  const [hoveredEntry, setHoveredEntry] = React.useState<number | null>(null);
-  const [mousePosition, setMousePosition] = React.useState({ x: 0, y: 0 });
-
-  const handleMouseEnter = (index: number) => {
-    setHoveredEntry(index);
-  };
-
-  const handleMouseLeave = () => {
-    setHoveredEntry(null);
-  };
-
-  const handleMouseMove = (event: React.MouseEvent) => {
-    setMousePosition({ x: event.clientX, y: event.clientY });
-  };
   // Creates a copy of the blogs list, possibly in reverse order
   let displayBlogs = reverseOrder ? [...blogs].reverse() : blogs;
 
@@ -45,16 +30,9 @@ const EntryList: React.FC<EntryListProps> = ({
       {displayBlogs.map((blog, index) => {
         // Calculates the correct index for links when order is reversed
         const linkIndex = reverseOrder ? blogs.length - 1 - index : index;
-        const isHovered = hoveredEntry === linkIndex;
 
         return (
-          <div
-            key={linkIndex}
-            className={entryList.entry}
-            onMouseEnter={() => handleMouseEnter(linkIndex)}
-            onMouseLeave={handleMouseLeave}
-            onMouseMove={handleMouseMove}
-          >
+          <div key={linkIndex} className={entryList.entry}>
             <div className={entryList.entryContent}>
               {/* Large NFT image on the left side of the entire entry */}
               {(blog.tokenID || blog.nftMetadata?.imageUrl) && (
@@ -80,17 +58,6 @@ const EntryList: React.FC<EntryListProps> = ({
               {/* Read more link */}
               <Link href={`${basePath}/${linkIndex}`}>Read more →</Link>
             </div>
-
-            {/* NFT image preview on hover */}
-            {(blog.tokenID || blog.nftMetadata?.imageUrl) && isHovered && (
-              <NFTPreviewImage
-                tokenId={blog.tokenID}
-                fallbackImageUrl={blog.nftMetadata?.imageUrl}
-                alt={blog.nftMetadata?.name || `NFT illustration for ${blog.title}`}
-                isVisible={isHovered}
-                position={mousePosition}
-              />
-            )}
           </div>
         );
       })}

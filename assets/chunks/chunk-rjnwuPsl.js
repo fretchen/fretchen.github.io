bb3038fc38b2491528ca3ac3d01ba6b9434d522b
fretchen <8323674+fretchen@users.noreply.github.com>
2025-06-25 05:51:48 +0000

    Deploying to gh-pages from @ fretchen/fretchen.github.io@8b4311038435eb5b4ba692ab64c75184de505b92 🚀

---
+++ assets/chunks/chunk-rjnwuPsl.js
@@ -0,0 +1,6 @@
+import{m as o}from"./chunk-szn2lhpv.js";import"./chunk-DHGBf05e.js";import"./chunk-C487EfNJ.js";import"./chunk-BpgHtCjF.js";/* empty css              *//* empty css              *//* empty css              */const e=o`<svg fill="none" viewBox="0 0 24 24">
+  <path
+    style="fill: var(--wui-color-accent-100);"
+    d="M10.2 6.6a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0ZM21 6.6a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0ZM10.2 17.4a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0ZM21 17.4a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0Z"
+  />
+</svg>`;export{e as allWalletsSvg};

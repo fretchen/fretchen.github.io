13bc0a36c6c0a666ec4460aa78f0906444f049cb
fretchen <polka.99gefiedert@icloud.com>
2025-09-24 16:45:22 +0200

    Update ImageGenerator.tsx

--- website/components/ImageGenerator.tsx
+++ website/components/ImageGenerator.tsx
@@ -9,7 +9,7 @@ import InfoIcon from "./InfoIcon";
 import { LocaleText } from "./LocaleText";
 import { useLocale } from "../hooks/useLocale";
 
-const defaultImageUrl = "https://mypersonaljscloudivnad9dy-readnftv2.functions.fnc.fr-par.scw.cloud";
+const defaultImageUrl = "https://mypersonaljscloudivnad9dy-genimgbfl.functions.fnc.fr-par.scw.cloud";
 
 // Helper function to wait for transaction confirmation
 export const waitForTransaction = async (hash: `0x${string}`): Promise<TransactionReceipt> => {

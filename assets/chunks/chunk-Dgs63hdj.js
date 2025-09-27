494c18ead4835468343574e57d193a7d1f97b581
fretchen <8323674+fretchen@users.noreply.github.com>
2025-09-27 20:39:57 +0000

    Deploying to gh-pages from @ fretchen/fretchen.github.io@95c5a5d507a10423afec7e2644b83d0caf5137b6 🚀

--- assets/chunks/chunk-Dgs63hdj.js
+++
@@ -1,8 +0,0 @@
-import{b as o}from"./chunk-B20ORCCx.js";import"./chunk-CT5atk_u.js";import"./chunk-Dt7qRG77.js";import"./chunk-Dv2Fn-IZ.js";/* empty css              *//* empty css              *//* empty css              */const m=o`<svg fill="none" viewBox="0 0 20 20">
-  <path
-    fill="currentColor"
-    fill-rule="evenodd"
-    d="M6.76.3a1 1 0 0 1 0 1.4L4.07 4.4h9a1 1 0 1 1 0 2h-9l2.69 2.68a1 1 0 1 1-1.42 1.42L.95 6.09a1 1 0 0 1 0-1.4l4.4-4.4a1 1 0 0 1 1.4 0Zm6.49 9.21a1 1 0 0 1 1.41 0l4.39 4.4a1 1 0 0 1 0 1.4l-4.39 4.4a1 1 0 0 1-1.41-1.42l2.68-2.68h-9a1 1 0 0 1 0-2h9l-2.68-2.68a1 1 0 0 1 0-1.42Z"
-    clip-rule="evenodd"
-  />
-</svg>`;export{m as swapHorizontalSvg};

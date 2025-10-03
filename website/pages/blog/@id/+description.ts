3bc69655ad441fa8f165df568f57fd56772e1939
fretchen <polka.99gefiedert@icloud.com>
2025-10-03 06:08:46 +0200

    Create +description.ts

---
+++ website/pages/blog/@id/+description.ts
@@ -0,0 +1,9 @@
+import blogs from "../../../blog/blogs.json";
+
+import type { PageContext } from "vike/types";
+
+export function description(pageContext: PageContext) {
+  const id = Number(pageContext.routeParams.id);
+  const blog = blogs[id];
+  return blog.description;
+}

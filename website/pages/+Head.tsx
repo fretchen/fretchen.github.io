0b53fb446212f76693520f04c1c71a820a77136f
fretchen <polka.99gefiedert@icloud.com>
2025-10-18 12:09:32 +0200

    Update +Head.tsx

--- website/pages/+Head.tsx
+++ website/pages/+Head.tsx
@@ -6,6 +6,10 @@ export default function HeadDefault() {
   return (
     <>
       <link rel="icon" href={favicon} />
+      {/* rel="me" link for identity verification (IndieWeb, Mastodon, etc.) */}
+      <link href="https://github.com/fretchen" rel="me" />
+      <link href="https://fretchen.bsky.social" rel="me atproto" />
+
       {/* umami analytics script - disabled when DISABLE_ANALYTICS is set */}
       {!import.meta.env.VITE_DISABLE_ANALYTICS && (
         <script

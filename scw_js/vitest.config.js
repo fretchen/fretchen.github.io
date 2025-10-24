486f72355cc6369f504ed1a1cb7f6b7d5dcc2c8f
fretchen <polka.99gefiedert@icloud.com>
2025-10-24 06:42:01 +0200

    Update vitest.config.js

--- scw_js/vitest.config.js
+++ scw_js/vitest.config.js
@@ -4,6 +4,12 @@ export default defineConfig({
   test: {
     environment: "node",
     globals: true,
+    pool: "threads",
+    poolOptions: {
+      threads: {
+        singleThread: true,
+      },
+    },
     coverage: {
       provider: "v8",
       reporter: ["text", "json", "html"],

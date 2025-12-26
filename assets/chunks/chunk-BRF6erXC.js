37ce253c0134f1888b88ec801537146879b88624
fretchen <polka.99gefiedert@icloud.com>
2025-12-26 14:28:42 +0000

    Deploying to gh-pages from @ fretchen/fretchen.github.io@8232967ed008871bd5ca8d73cec5c7d92fae4adc 🚀

---
+++ assets/chunks/chunk-BRF6erXC.js
@@ -0,0 +1,1 @@
+import{b as r}from"./chunk-BSA-s7wg.js";var e=4;function a(o){return r(o,e)}export{a as c};
---
+++ assets/chunks/chunk-Bn6zj5IC.js
@@ -0,0 +1,6 @@
+import{b as o}from"./chunk-DJYWiRlG.js";import"./chunk-t-pXVLBL.js";import"./chunk-CKQjEm_g.js";import"./chunk-D8AR0Meb.js";/* empty css              *//* empty css              */const m=o`<svg fill="none" viewBox="0 0 24 24">
+  <path
+    style="fill: var(--wui-color-accent-100);"
+    d="M10.2 6.6a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0ZM21 6.6a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0ZM10.2 17.4a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0ZM21 17.4a3.6 3.6 0 1 1-7.2 0 3.6 3.6 0 0 1 7.2 0Z"
+  />
+</svg>`;export{m as allWalletsSvg};

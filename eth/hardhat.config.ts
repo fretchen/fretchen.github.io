3cd2b3c3b4f62d3ee943eca7d607007f08ba6045
fretchen <polka.99gefiedert@icloud.com>
2026-01-04 08:43:52 +0100

    Update solidity

--- eth/hardhat.config.ts
+++ eth/hardhat.config.ts
@@ -14,7 +14,7 @@ const SEPOLIA_PRIVATE_KEY: string = vars.get(
 const ETHERSCAN_API_KEY: string = vars.get("ETHERSCAN_API_KEY", "dummy-etherscan-key");
 
 const config = {
-  solidity: "0.8.28",
+  solidity: "0.8.33",
   gasReporter: {
     enabled: false,
   },

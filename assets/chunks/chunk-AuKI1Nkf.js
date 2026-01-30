1ce8bf4864f673db50fb683059f54c7cda927d1b
fretchen <polka.99gefiedert@icloud.com>
2026-01-30 14:25:23 +0000

    Deploying to gh-pages from @ fretchen/fretchen.github.io@22f1f484381a10f3795cbc79cc44c19157a7103b 🚀

--- assets/chunks/chunk-AuKI1Nkf.js
+++
@@ -1,3 +0,0 @@
-import{b as o}from"./chunk-DKPfQ48k.js";import"./chunk-t-pXVLBL.js";import"./chunk-BFjvKHaM.js";import"./chunk-CNICcFgQ.js";/* empty css              *//* empty css              */const e=o` <svg fill="none" viewBox="0 0 13 4">
-  <path fill="currentColor" d="M.5 0h12L8.9 3.13a3.76 3.76 0 0 1-4.8 0L.5 0Z" />
-</svg>`;export{e as cursorSvg};

3b0bcd8beb9c5c4c7d2359c3b390d28bb9a8cbf4
fretchen <polka.99gefiedert@icloud.com>
2026-03-15 15:33:04 +0000

    Deploying to gh-pages from @ fretchen/fretchen.github.io@be9c0a2a79179dd36e85cfd9c37ed641ad0a16b2 🚀

---
+++ assets/chunks/chunk-CC0ZFX1r.js
@@ -0,0 +1,3 @@
+import{b as o}from"./chunk-C088IT-v.js";import"./chunk-t-pXVLBL.js";import"./chunk-C3IfZUiz.js";import"./chunk-Dyk_8Ffb.js";/* empty css              *//* empty css              */const e=o` <svg fill="none" viewBox="0 0 13 4">
+  <path fill="currentColor" d="M.5 0h12L8.9 3.13a3.76 3.76 0 0 1-4.8 0L.5 0Z" />
+</svg>`;export{e as cursorSvg};
---
+++ assets/chunks/chunk-Cryusnx6.js
@@ -0,0 +1,1 @@
+import{b as r}from"./chunk-DAGnoFjN.js";var e=4;function a(o){return r(o,e)}export{a as c};
